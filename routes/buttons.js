5e05b4b590afb09be431d1db8dfbed7a394b3404
JordanJWSmith <jordanjwmsith@gmail.com>
2021-07-24 15:21:26 +0100

    functioning login

--- routes/buttons.js
+++ routes/buttons.js
@@ -4,25 +4,25 @@ var cookieParser = require('cookie-parser');
 
 
 
-/* GET buttons page. */
-router.get('/', function(req, res, next) {
-//   console.log('cookies: ', req.cookies);
-//   console.log(Object.keys(req.cookies));
-//   console.log(Object.keys(req.cookies).includes('msal.idtoken'));
-  var cookieArray = Object.keys(req.cookies);
-  var logIn = (cookieArray.includes('loginID'));
-  if (logIn) {
-    res.render('buttons', { title: 'Only Accessible via login', logIn: logIn });
-  } else {
-    res.redirect('/');
-  }
+// /* GET buttons page. */
+// router.get('/', function(req, res, next) {
+// //   console.log('cookies: ', req.cookies);
+// //   console.log(Object.keys(req.cookies));
+// //   console.log(Object.keys(req.cookies).includes('msal.idtoken'));
+//   var cookieArray = Object.keys(req.cookies);
+//   var logIn = (cookieArray.includes('loginID'));
+//   if (logIn) {
+//     res.render('buttons', { title: 'Only Accessible via login', logIn: logIn });
+//   } else {
+//     res.redirect('/');
+//   }
 
   
-//   console.log(cookieParser.JSONCookies(req.cookies));
-//   console.log((cookieParser.JSONCookies(req.cookies) == req.cookies));
+// //   console.log(cookieParser.JSONCookies(req.cookies));
+// //   console.log((cookieParser.JSONCookies(req.cookies) == req.cookies));
   
 
-});
+// });
 
 
 
