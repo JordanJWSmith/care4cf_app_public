36c2297df9a3851be7e05de1b4dd35df8440cde7
JordanJWSmith <jordanjwmsith@gmail.com>
2021-08-09 10:50:30 +0100

    added unit tests

--- routes/appDatabase/getTechniques.js
+++ routes/appDatabase/getTechniques.js
@@ -2,6 +2,8 @@ var readData = require('./readData');
 
 module.exports = async function() {
 
+    
+
         var checkLogin =  "SELECT * FROM techdescriptions WHERE techniqueType = ? OR techniqueType = ? ORDER BY techniqueType DESC";
         var data = ['Device-Free', 'Device-Dependent'];
         var results = await readData(checkLogin, data);
---
+++ test/getRoutineTypes.js
@@ -0,0 +1,19 @@
+var getRoutineTypes = require('../routes/appDatabase/getRoutineTypes.js');
+var expect = require('chai').expect;
+
+describe('#getRoutineTypes()', function() {
+    var args = Array.prototype.slice.call(arguments);
+
+
+    context('with no arguments', function() {
+        it('should return object', async function () {
+            expect(
+                await getRoutineTypes().then(
+                    function(results) {
+                        return results
+                    }
+                )
+            ).to.be.an('object');
+        })
+    })
+})
