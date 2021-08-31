1d374453ce7c3873556c4a2ffe18cd4f257c075f
JordanJWSmith <jordanjwmsith@gmail.com>
2021-08-31 20:44:12 +0100

    added loading message on pagination

---
+++ public/javascripts/checkLocal.js
@@ -0,0 +1,14 @@
+function checkLocal() {
+    if ((!update) && ((!(window.localStorage.getItem('routineDict'))) 
+    || (!(window.localStorage.getItem('allNormals'))) 
+    || (!(window.localStorage.getItem('allActivities'))) 
+    || (!(window.localStorage.getItem('dateList'))))) {
+        var firstUpdate = true;
+        console.log('firstUpdate');
+        document.cookie = "dataUpdate=true";
+        document.getElementById('mainSection').innerHTML = 'Please wait...'
+        // window.sessionStorage.setItem('dataUpdate', 'true');
+        window.location = "/";
+        // return true;
+    }
+}
